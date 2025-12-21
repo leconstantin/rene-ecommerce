eee44b38b591a2f194cd946d1a85664836194797
Leo Constantin <lcon69184@gmail.com>
2025-12-21 13:00:15 +0200

    added normal product page

---
+++ app/(web)/product/[handle]/page.tsx
@@ -0,0 +1,169 @@
+import type { Metadata } from "next";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
+import { GridTileImage } from "@/features/grid/tile";
+import { Gallery } from "@/features/product/gallery";
+import { ProductProvider } from "@/features/product/product-context";
+import { ProductDescription } from "@/features/product/product-description";
+import { HIDDEN_PRODUCT_TAG } from "@/shopify/constants";
+import { getProduct, getProductRecommendations } from "@/shopify/index";
+import type { Image as ImageType } from "@/shopify/types";
+
+export async function generateMetadata(props: {
+  params: Promise<{ handle: string }>;
+}): Promise<Metadata> {
+  const params = await props.params;
+  const product = await getProduct(params.handle);
+
+  if (!product) {
+    return notFound();
+  }
+
+  const { url, width, height, altText: alt } = product.featuredImage || {};
+  const indexable = !product.tags.includes(HIDDEN_PRODUCT_TAG);
+
+  return {
+    title: product.seo.title || product.title,
+    description: product.seo.description || product.description,
+    robots: {
+      index: indexable,
+      follow: indexable,
+      googleBot: {
+        index: indexable,
+        follow: indexable,
+      },
+    },
+    openGraph: url
+      ? {
+          images: [
+            {
+              url,
+              width,
+              height,
+              alt,
+            },
+          ],
+        }
+      : null,
+  };
+}
+// export async function generateStaticParams() {
+//   const products = await getProducts({ query: "" });
+
+//   return products.slice(0, 5).map((p) => ({
+//     handle: p.handle,
+//   }));
+// }
+export default function ProductPage(props: PageProps<"/product/[handle]">) {
+  return (
+    <Suspense fallback={null}>
+      <SuspendedProduct {...props} />
+    </Suspense>
+  );
+}
+async function SuspendedProduct(props: PageProps<"/product/[handle]">) {
+  const params = await props.params;
+  const product = await getProduct(params.handle);
+
+  if (!product) {
+    return notFound();
+  }
+
+  const productJsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Product",
+    name: product.title,
+    description: product.description,
+    image: product.featuredImage.url,
+    offers: {
+      "@type": "AggregateOffer",
+      availability: product.availableForSale
+        ? "https://schema.org/InStock"
+        : "https://schema.org/OutOfStock",
+      priceCurrency: product.priceRange.minVariantPrice.currencyCode,
+      highPrice: product.priceRange.maxVariantPrice.amount,
+      lowPrice: product.priceRange.minVariantPrice.amount,
+    },
+  };
+  return (
+    <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(productJsonLd),
+        }}
+        type="application/ld+json"
+      />
+      <div className="container mx-auto w-full max-w-[1536px] pt-4">
+        <ProductProvider>
+          <div className="flex flex-col rounded-lg border border-neutral-200 bg-white lg:flex-row dark:border-neutral-800 dark:bg-black">
+            <div className="h-full w-full basis-full border-neutral-200 border-r px-6 lg:basis-3/5 dark:border-neutral-800">
+              <Suspense
+                fallback={
+                  <div className="relative aspect-square h-full min-h-[550px] w-full overflow-hidden" />
+                }
+              >
+                <Gallery
+                  images={product.images
+                    .slice(0, 5)
+                    .map((image: ImageType) => ({
+                      src: image.url,
+                      altText: image.altText,
+                    }))}
+                />
+              </Suspense>
+            </div>
+
+            <div className="basis-full p-6 lg:basis-2/5">
+              <Suspense fallback={null}>
+                <ProductDescription product={product} />
+              </Suspense>
+            </div>
+          </div>
+        </ProductProvider>
+
+        <RelatedProducts id={product.id} />
+      </div>
+    </>
+  );
+}
+
+async function RelatedProducts({ id }: { id: string }) {
+  const relatedProducts = await getProductRecommendations(id);
+
+  if (!relatedProducts.length) {
+    return null;
+  }
+
+  return (
+    <div className="py-8">
+      <h2 className="mb-4 font-bold text-2xl">Related Products</h2>
+      <ul className="flex w-full gap-4 overflow-x-auto pt-1">
+        {relatedProducts.map((product) => (
+          <li
+            className="aspect-square w-full flex-none sm:w-1/3 md:w-1/4 lg:w-1/5 min-[475px]:w-1/2"
+            key={product.handle}
+          >
+            <Link
+              className="relative h-full w-full"
+              href={`/product/${product.handle}`}
+              prefetch={true}
+            >
+              <GridTileImage
+                alt={product.title}
+                fill
+                label={{
+                  title: product.title,
+                  amount: product.priceRange.maxVariantPrice.amount,
+                  currencyCode: product.priceRange.maxVariantPrice.currencyCode,
+                }}
+                sizes="(min-width: 1024px) 20vw, (min-width: 768px) 25vw, (min-width: 640px) 33vw, (min-width: 475px) 50vw, 100vw"
+                src={product.featuredImage?.url}
+              />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
